fe7e33370bed84906e2941e2600c097c4214bc6d
Ebenholza <110331060+Ebenholza@users.noreply.github.com>
2022-07-31 18:58:34 +0800

    rename

--- main.go
+++ main.go
@@ -201,8 +201,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	sus = append(sus, 1050795713)
-	// sus = append(sus, 87654321)
+	//sus = append(sus, 1050795713)
+	sus = append(sus, 2744482259)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
